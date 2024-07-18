6916258d415137416886f1cbf861c39ea9b4454d
Leonardo Rodrigues <vr.leonardo2020@gmail.com>
2024-07-18 16:52:18 -0300

    Update script-mensagem.js

--- script-mensagem.js
+++ script-mensagem.js
@@ -1,5 +1,5 @@
 const html = document.querySelector('html');
 
-if (window.matchMedia((prefers-color-scheme dark)).matches) {
+if (window.matchMedia(('prefers-color-scheme dark')).matches) {
     html.classList.add('dark-mode');
 }
